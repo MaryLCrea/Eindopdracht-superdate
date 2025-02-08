6cccfd8f35c6c7cd7350bd37da164f2c10130f0d
MaryLCrea <maryleenen@yahoo.com>
2025-02-08 17:20:56 +0100

    plaatje+tekst+css aangepast

--- src/pages/notfound/NotFound.js
+++ src/pages/notfound/NotFound.js
@@ -2,6 +2,7 @@ import React from "react";
 import HomeHeader from "../../components/homeheader/HomeHeader";
 import {Link} from "react-router-dom";
 import './NotFound.css';
+import picture from "../../assets/dog.png";
 
 function NotFound() {
 
@@ -11,9 +12,13 @@ function NotFound() {
             <main className="outer-page-container">
                 <div className="inner-page-container pages">
 
-                    <h2>Oops... This page doesn't exist</h2>
+                    <h2>Oops... The page you are trying to find doesn't exist</h2>
+
+                    <div className="container">
+                    <img src={picture} className="dog" alt="dog"/>
+                    </div>
                     <p className="link-text">
-                    <Link to="/">Take me back to the home page</Link>
+                        <Link to="/" className="home-link">⬅ Back to Home</Link>
                     </p>
                 </div>
             </main>
